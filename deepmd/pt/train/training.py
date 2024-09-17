7ab5b17f9fe508bfbaa6591dd3210e889b6287f3
anyangml <anyangpeng.ca@gmail.com>
2024-09-18 03:39:35 +0800

    support multitask finetune

--- deepmd/pt/train/training.py
+++ deepmd/pt/train/training.py
@@ -586,35 +586,43 @@ class Trainer:
 
         # TODO add optimizers for multitask
         # author: iProzd
-        embed()
         if config["learning_rate"]["repformer_lr_factor"] is not None:
-            layerwise
-            assert config["model"]["descriptor"]["type"] == "dpa2"
-            layer_names = []
-            for (name, param) in self.wrapper.named_parameters():
-                layer_names.append(name)
-            layer_names.reverse()
-            lr = config["learning_rate"]["start_lr"]
-            lr_mult = 0.9
-            parameters = []
-            names = [] #tmp
-            prev_group_name = ".".join(layer_names[0].split(".")[:7])
+            if isinstance(config["learning_rate"]["repformer_lr_factor"], float):
+                #assert (config["model"]["descriptor"]["type"] == "dpa2") or (config["model"]["shared_dict"]["dpa2_descriptor"]["type"] == "dpa2"
+                total_layer_names = []
+                layer_names = []
+                for (name, param) in self.wrapper.named_parameters():
+                    total_layer_names.append(name)
+                for (name, param) in self.wrapper.named_parameters():
+                    if ("repinit" in name):
+                        layer_names.append(name)
+                for (name, param) in self.wrapper.named_parameters():
+                    if ("repinit" not in name):
+                        layer_names.append(name)
+                layer_names.reverse()
+                lr = config["learning_rate"]["start_lr"]
+                lr_mult = config["learning_rate"]["repformer_lr_factor"]
+                parameters = []
+                names = [] #tmp
+                prev_group_name = ".".join(layer_names[0].split(".")[:7])
 
-            for name in layer_names:
-                cur_group_name = ".".join(name.split(".")[:7])
-                if cur_group_name != prev_group_name and "repformers.layers" in cur_group_name:
-                    lr *= lr_mult
-                prev_group_name = cur_group_name
-                parameters += [{"params": [p for n,p in self.wrapper.named_parameters() if n==name and p.requires_grad],
-                                "lr": lr}]
-                names += [{"name": name,
-                        "lr": lr}]
+                for name in layer_names:
+                    cur_group_name = ".".join(name.split(".")[:7])
+                    if cur_group_name != prev_group_name and "repformers.layers" in cur_group_name:
+                        lr *= lr_mult
+                    prev_group_name = cur_group_name
+                    parameters += [{"params": [p for n,p in self.wrapper.named_parameters() if n==name and p.requires_grad],
+                                    "lr": lr}]
+                    names += [{"name": name,
+                            "lr": lr}]
 
         if self.opt_type == "Adam":
-            self.optimizer = torch.optim.Adam(
-                self.wrapper.parameters(), lr=self.lr_exp.start_lr
-            )
-            #self.optimizer = torch.optim.Adam(parameters)
+            if config["learning_rate"]["repformer_lr_factor"] is None:
+                self.optimizer = torch.optim.Adam(
+                    self.wrapper.parameters(), lr=self.lr_exp.start_lr
+                )
+            else:
+                self.optimizer = torch.optim.Adam(parameters)
             if optimizer_state_dict is not None and self.restart_training:
                 self.optimizer.load_state_dict(optimizer_state_dict)
             self.scheduler = torch.optim.lr_scheduler.LambdaLR(
